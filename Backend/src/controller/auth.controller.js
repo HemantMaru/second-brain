29cc0d88d7697b0074a134af406c4d99c797f7b5
Hemant Maru <hemantkumawat399@gmail.com>
2026-04-05 23:33:45 +0530

    Fix auth cookie for production

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -8,10 +8,10 @@ const sendTokenResponse = (user, statusCode, res) => {
   });
 
   const cookieOptions = {
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
   };
 
   res
