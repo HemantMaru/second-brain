b55fbe85cf14c0b827ed4266dc4edb6c9de305cf
Hemant Maru <hemantkumawat399@gmail.com>
2026-04-05 23:25:29 +0530

    Fix CORS correctly

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,24 +19,15 @@ import router from "./routes/auth.routes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser"; // 👈 Ye line missing hai
 const app = express();
-app.use("/uploads", express.static("uploads"));
-app.use("/uploads", express.static("public/uploads"));
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Pro touch: form data handle karne ke liye
-app.get("/", (req, res) => {
-  res.send("Server working");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Test route working");
-});
 app.use(
   cors({
     // "*" ke saath credentials: true kabhi-kabhi issue deta hai
     // Isliye specific origins dalna hamesha better hai
     origin: function (origin, callback) {
-      const allowedOrigins = [ "https://frontend-pz3b1ci56-hemantkumawat399-5351s-projects.vercel.app" "null"]; // Extension 'null' ya specific ID bhejti hai
+      const allowedOrigins = [
+        "https://frontend-pz3b1ci56-hemantkumawat399-5351s-projects.vercel.app",
+        "null",
+      ]; // Extension 'null' ya specific ID bhejti hai
       if (
         !origin ||
         allowedOrigins.indexOf(origin) !== -1 ||
@@ -50,6 +41,18 @@ app.use(
     credentials: true,
   }),
 );
+app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("public/uploads"));
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Pro touch: form data handle karne ke liye
+app.get("/", (req, res) => {
+  res.send("Server working");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test route working");
+});
 
 app.use(cookieParser());
 
