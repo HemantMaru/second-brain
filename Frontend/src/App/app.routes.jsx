a0e3900744debac5a7c3e6471bddf1e1f427fd5d
Hemant Maru <hemantkumawat399@gmail.com>
2026-04-06 13:49:34 +0530

    changeroutes

--- Frontend/src/App/app.routes.jsx
+++ Frontend/src/App/app.routes.jsx
@@ -35,10 +35,6 @@ export const routes = createHashRouter([
   },
   {
     path: "/",
-    element: (
-      <ProtectedRoute>
-        <CreateNode />
-      </ProtectedRoute>
-    ),
+    element: <CreateNode />,
   },
 ]);
