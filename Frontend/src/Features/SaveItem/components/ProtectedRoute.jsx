5d5dab75682cbb518ab010b97d63661092397d2a
Hemant Maru <hemantkumawat399@gmail.com>
2026-04-06 21:54:09 +0530

    complete

--- Frontend/src/Features/SaveItem/components/ProtectedRoute.jsx
+++ Frontend/src/Features/SaveItem/components/ProtectedRoute.jsx
@@ -4,7 +4,13 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
 
   return isAuthenticated ? children : <Navigate to="/auth" replace />;
 };
